2752f2d38a0c95895fb5708f31a4398b662c2a9e
Arjun <simplegamerofficial97@gmail.com>
2024-10-06 20:42:10 +0530

    Modified CSS & HTML. Changed JS to loop over array of 10, insteded of calling API every question

--- quiz.js
+++ quiz.js
@@ -1,84 +1,89 @@
 //https://opentdb.com/api.php?amount=1&category=9&type=multiple
 
 const container = document.querySelector('.container');
+const question = document.querySelector('.question');
+const category = document.querySelector('.category');
+const choiceContainer = document.querySelector('.choice-container');
+const nextBtn = document.querySelector('.next-btn');
+const viewAnswerBtn = document.querySelector('.view-answer-btn');
+const finishQuizBtn = document.querySelector('.finish-quiz-btn');
 
-getQuestions();
+handleQuestionData();
+
+finishQuizBtn.addEventListener('click', () => {
+  container.innerHTML = `<h1> Congrats! you finished the quiz.</h1>
+  <h3 class="finish-msg">Wanna Play again? <button id="click-me-btn">Click me</botton></h3>`;
+  playAgain();
+});
+
+function playAgain(){
+  const clickMeBtn = document.getElementById('click-me-btn');
+  clickMeBtn.addEventListener('click', () => {
+    console.log("hi");
+    location.reload();
+  });
+}
+
+async function handleQuestionData(){
+  const questionsArray = await getQuestions();
+  let currentIndex = 0;
+  //let wrongAnswers = 0;
+  //let rightAnswers = 0;
+
+  displayQuestion();
+
+  function displayQuestion() {
+    if (currentIndex >= questionsArray.length) {
+      //alert("Quiz is over");
+      nextBtn.style.display = 'none';
+      finishQuizBtn.style.display = 'inline-block';
+
+    } else {
+      const questionData = questionsArray[currentIndex].question;
+      const categoryData = questionsArray[currentIndex].category;
+      const correctAnswer = questionsArray[currentIndex].correct_answer;
+      const incorrectAnswers = questionsArray[currentIndex].incorrect_answers;//this is in nested array format
+      //console.log(incorrectAnswers);
+      incorrectAnswers.splice(Math.floor(Math.random() *(incorrectAnswers.length + 1)), 0, correctAnswer);
+      // to place correct answer in random position of any array 
+      //console.log(incorrectAnswers);
+
+      question.innerHTML = `<p class="question">${questionData}</p>`;
+      category.innerHTML = `<p class="category"><span>Category:</span> <span>${categoryData}</span></p>`
+      choiceContainer.innerHTML = `${incorrectAnswers.map((item) =>`
+      <li class="choice"> ${item}</li>`).join(" ")}`;
+      viewAnswerBtn.innerText = "View Answer";
+      nextBtn.innerText = "Next";
+    
+      checkCorrectAnswer(correctAnswer);
+      currentIndex++;
+    }
+  }
+
+  nextBtn.addEventListener('click', displayQuestion);
+  nextBtn.addEventListener('click', () => {
+    viewAnswerBtn.style.display = 'none';//to hide viewbtn when clicked on next.
+  });
+}
 
 async function getQuestions(){
   try{
-    const response = await fetch("https://opentdb.com/api.php?amount=1");
+    const response = await fetch("https://opentdb.com/api.php?amount=10");
     const data = await response.json();
-    const questionData = data.results[0];
-    displayQuestions(questionData);
-
+    //const questionData = data.results[0];
+    const questionData = data.results;
+    //console.log(questionData);
+    //console.log(questionData.length);
+    //displayFirstQuestion(questionData);
+    
     if(!response.ok){
       throw new Error("Could not find resource!");
     }
+    return questionData;
   }
   catch(err){
     console.error(err);
   }
-}
-
-async function displayQuestions(data) {
-  console.log(data);
-  const questionData = data.question;
-  const categoryData = data.category;
-  const correctAnswer = data.correct_answer;
-  //const {questionData, categoryData, correctAnswer} = data[question,category,correct_answer];
-  const incorrectAnswers = data.incorrect_answers; 
-  //this will be in array format
-  incorrectAnswers.splice(Math.floor(Math.random() *(incorrectAnswers.length + 1)), 0, correctAnswer);
-  // to place correct answer in random position of any array 
-  //console.log(incorrectAnswers);
-  console.log(correctAnswer);
-
-  createDomItems();
-
-  const question = document.querySelector('.question');
-  const category = document.querySelector('.category');
-  const choiceContainer = document.querySelector('.choice-container');
-  const nextBtn = document.querySelector('.next-btn');
-  const viewAnswerBtn = document.querySelector('.view-answer-btn');
-
-  question.innerHTML = `<p class="question">${questionData}</p>`;
-  category.innerHTML = `<p class="category"><span>Category:</span> <span>${categoryData}</span></p>`
-  choiceContainer.innerHTML = `${incorrectAnswers.map((item) =>`
-  <li class="choice"> ${item}</li>`).join(" ")}`;
-  
-  viewAnswerBtn.innerText = "View Answer";
-  nextBtn.innerText = "Next";
-
-  checkCorrectAnswer(correctAnswer);
-  activeNextBtnListener();
-
-}
-
-function createDomItems(){
-  const question = document.createElement('h2');
-  question.classList.add('question');
-  container.appendChild(question);
-
-  const category = document.createElement('p');
-  category.classList.add('category');
-  container.appendChild(category);
-
-  const choiceContainer = document.createElement('ul');
-  choiceContainer.classList.add('choice-container');
-  container.appendChild(choiceContainer);
-
-  const bottomDiv = document.createElement('div');
-  bottomDiv.classList.add('bottom-div');
-  container.appendChild(bottomDiv);
-
-  const nextBtn = document.createElement('button');
-  nextBtn.classList.add('next-btn');
-  bottomDiv.appendChild(nextBtn);
-
-  const viewAnswerBtn = document.createElement('button');
-  viewAnswerBtn.classList.add('view-answer-btn');
-  bottomDiv.appendChild(viewAnswerBtn);
-
 }
 
 function checkCorrectAnswer(correctAnswer){
@@ -90,11 +95,12 @@ function checkCorrectAnswer(correctAnswer){
       if(item.innerText == correctAnswer){
         console.log("Answer is correct")
         item.classList.add('correct');
+        //rightAnswers++;
       }else{
         console.log("Wrong answer")
         item.classList.add('wrong');
-        const viewAnswerBtn = document.querySelector('.view-answer-btn');
         viewAnswerBtn.style.display = 'inline-block';
+        //wrongAnswers++;
       }
       // Disable clicks for the rest of the <li> elements
       choices.forEach((otherchoices, otherIndex) => {
@@ -104,13 +110,5 @@ function checkCorrectAnswer(correctAnswer){
         }
       });
     });
-  });
-}
-
-function activeNextBtnListener(){
-  const nextBtn = document.querySelector('.next-btn');
-
-  nextBtn.addEventListener('click', () => {
-      location.reload(); // Reloads the page
   });
 }
